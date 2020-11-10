e814c78da30dfbf5f5dbeb4ef992357ae910a18e
NuttySalmon <nuttysalmon@gmail.com>
2020-11-09 21:34:27 -0800

    Converted elements into Bootstrap.

--- frontend/src/components/authorization/SignUp.js
+++ frontend/src/components/authorization/SignUp.js
@@ -1,50 +1,78 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react'
+import { Button, Col, Form, Row } from 'react-bootstrap'
+import FormContainer from '../../layout/FormContainer'
+import style from '../../layout/css/formContainer.module.css'
 
-class SignUp extends Component {
+const SignUp = () => {
+  const handleSubmit = () => {}
+  const [userInfo, setUserInfo] = useState({
+    firstName: '',
+    lastName: '',
+    username: '',
+    password: '',
+  })
 
-  state= {
-    firstName: '', 
-    lastName:'',
-    email:'',
-    password:''
+  const handleOnChange = (event, field) => {
+    setUserInfo({ ...userInfo, [field]: event.target.value })
   }
 
-  
+  return (
+    <FormContainer>
+      <Col>
+        <h2> Sign Up </h2>
+      </Col>
+      <Col className={style.form}>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Control
+              type="text"
+              placeholder="First name"
+              value={userInfo.firstName}
+              onChange={e => {
+                handleOnChange(e, 'firstName')
+              }}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Control
+              type="text"
+              placeholder="Last name"
+              value={userInfo.lastName}
+              onChange={e => {
+                handleOnChange(e, 'lastName')
+              }}
+            />
+          </Form.Group>
 
-  render() {
-    return (
-      <div className='container'>
-        <form className='white' onSubmit={this.handleSubmit} >
-          <h4 className='grey-text text-darken-3'>Sign Up</h4>
+          <Form.Group>
+            <Form.Control
+              style={{marginBottom: '0'}}
+              type="text"
+              placeholder="Pick a username"
+              value={userInfo.username}
+              onChange={e => {
+                handleOnChange(e, 'username')
+              }}
+            />
+            <Form.Text muted className="mt-0">Will be used for sign in.</Form.Text>
+          </Form.Group>
 
-          <div className='input-field'>
-            <label htmlFor='firstName'>First Name</label>
-            <input type='text' id='firstName' onChange={this.handleChange}/>
-          </div>
-
-          <div className='input-field'>
-            <label htmlFor='lastName'>Last Name</label>
-            <input type='text' id='lastName' onChange={this.handleChange} />
-          </div>
-
-          <div className='input-field'>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' onChange={this.handleChange} />
-          </div>
-
-          <div className='input-field'>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' onChange={this.handleChange} />
-          </div>
-
-          <div className='input-field'>
-            <button className='btn pink lighten-1 z-depth-0'>Login
-            </button>
-          </div>
-        </form>
-      </div>
-    )
-  }
+          <Form.Group>
+            <Form.Control
+              style={{marginTop: '1em'}}
+              type="password"
+              value={userInfo.password}
+              onChange={e => {
+                handleOnChange(e, 'password')
+              }}
+              placeholder="Enter password"
+            />
+          </Form.Group>
+          <Button>Submit</Button>
+        </Form>
+      </Col>
+    </FormContainer>
+  )
 }
 
-export default SignUp; 
+export default SignUp
--- frontend/src/components/projects/createProject.js
+++ frontend/src/components/projects/createProject.js
@@ -1,56 +1,82 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
+import { Form, Col, Button, Row } from 'react-bootstrap'
+import FormContainer from '../../layout/FormContainer'
+import style from '../../layout/css/formContainer.module.css'
+const CreateProject = () => {
+  const [storyData, setProjectData] = useState({
+    name: '',
+    sdate: '',
+    edate: '',
+    progress: '',
+  })
 
-class createProject extends Component{
-  
-  state = {
-    pname:'', 
-    sdate: '', 
-    edate: '', 
-    progress: ''
+  const handleSubmit = () => {}
+
+  const handleChange = (event, field) => {
+    let value = event.target.value
+    if (field === 'progress') {
+      if (value > 100) value = 100
+      if (value < 0) value = 0
+    }
+    setProjectData({
+      ...storyData,
+      [field]: value,
+    })
   }
 
+  return (
+    <FormContainer>
+      <Row>
+        <h2>New Story</h2>
+      </Row>
+      <Row>
+        <Form onSubmit={handleSubmit} className={style.form}>
+          <Form.Group>
+            <Form.Control
+              type="text"
+              value={storyData.name}
+              onChange={e => {
+                handleChange(e, 'name')
+              }}
+              placeholder="Story name"
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Control
+              type="date"
+              value={storyData.sdate}
+              onChange={e => {
+                handleChange(e, 'sdate')
+              }}
+              placeholder="Start date"
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Control
+            className="dark"
+              type="date"
+              value={storyData.edate}
+              onChange={e => {
+                handleChange(e, 'edate')
+              }}
+              placeholder="End date"
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Control
+              type="number"
+              value={storyData.progress}
+              onChange={e => {
+                handleChange(e, 'progress')
+              }}
+              placeholder="Progress %"
+            />
+          </Form.Group>
 
-  handleSubmit = ()=> {
-  }
-
-  handleChange = () => {
-
-  }
-
-
-
-  render() {
-    return (
-      <div className='container'>
-        <form className='white' onSubmit={this.handleSubmit}>
-          <h4 className='grey-text text-darken-3'>Create Project</h4>
-          <div className='input-field'>
-            <label  htmlFor='pname'>Project Name</label>
-            <input type='text' id='pname' onChange={this.handleChange} />
-          </div>
-
-          <div className='input-field'>
-            <label htmlFor='sdate' />
-            <input type='date' id='sdate' onChange={this.handleChange} />
-          </div>
-
-          <div className='input-field'>
-            <label htmlFor='edate'/>
-            <input type='date' id='edate' onChange={this.handleChange} />
-          </div>
-
-          <div className='input-field'>
-            <label htmlFor='progress'>Project Progress</label>
-            <input type='number' id='progress' onChange={this.handleChange} />
-          </div>
-
-          <div className='input-field'>
-            <button className='btn pink lighten-2 z-depth-0'>Create</button> 
-          </div>
-        </form> 
-      </div>
-    )
-  }
+          <Button>Create</Button>
+        </Form>
+      </Row>
+    </FormContainer>
+  )
 }
-
-export default createProject
+export default CreateProject
