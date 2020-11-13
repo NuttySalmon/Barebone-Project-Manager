5e92a9a7c97fffafb7dbc26f4e89cd0363554b54
Raza Ghulam <ahmad.abdali001@gmail.com>
2020-11-13 10:58:13 -0800

    Sign In changed

--- frontend/src/components/authorization/SignIn.js
+++ frontend/src/components/authorization/SignIn.js
@@ -10,34 +10,8 @@ const SignIn = () => {
   const onChangeUsername = e => {
     setUsername(e.target.value)
   }
-<<<<<<< HEAD
-  
-  render() {
-    return (
-      <div className='container '>
-        <form className='white' onSubmit={this.handleSubmit}>
-          <h4 className='grey-text text-darken-3'>Sign In</h4>
-          
-          <div className="input-field">
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' onChange= {this.handleChange} />
-          </div>
-
-          <div className="input-field">
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' onChange={this.handleChange} />
-          </div>
-
-          <div className='input-field'>
-            <button className='btn pink light-2 z-depth-0'>Log In</button>
-          </div> 
-        </form>
-      </div>
-    )
-=======
   const onChangePassword = e => {
     setPassword(e.target.value)
->>>>>>> 4d2d6a0945974ad1135884cb6d8b29d216daae15
   }
   return (
     <AuthForm>
