07ce4404c90f00534278511a9325154cef1e17ea
NuttySalmon <nuttysalmon@gmail.com>
2020-11-19 06:08:22 -0800

    Changed form container title  to use primary color.

--- frontend/src/layout/FormContainer.js
+++ frontend/src/layout/FormContainer.js
@@ -26,6 +26,7 @@ const useStyles = makeStyles(theme => ({
   title: {
     textAlign: 'center',
     alignSelf: 'center',
+    color: theme.palette.primary.main,
     [theme.breakpoints.down('sm')]: {
       textAlign: 'left',
     },
