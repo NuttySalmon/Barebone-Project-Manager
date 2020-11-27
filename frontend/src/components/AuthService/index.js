6aaa3c3d2c7c331484c8d238fe4aeab66dae263c
NuttySalmon <nuttysalmon@gmail.com>
2020-11-27 03:29:04 -0800

    Added username field in context

--- frontend/src/components/AuthService/index.js
+++ frontend/src/components/AuthService/index.js
@@ -7,18 +7,37 @@ import { Route, useHistory } from 'react-router-dom'
 export const UserContext = createContext()
 const AuthService = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem('token'))
+  const [username, setUsername] = useState(localStorage.getItem('username'))
+
   const history = useHistory()
   useEffect(() => {
     localStorage.setItem('token', token)
   }, [token])
+
+  useEffect(() => {
+    localStorage.setItem('username', username)
+  }, [username])
+
+  /**
+   * Sign out logic. Set token to empty string and remove move from local storage.
+   * Finally redirect to sign in page
+   */
   const setSignOut = () => {
     setToken('')
+    setUsername('')
     localStorage.removeItem('token')
     history.push('/signin')
   }
+
+  /**
+   * Get authorization header to be used with Axio
+   */
   const getAuthHeader = () => ({ Authorization: `Bearer ${token}` })
+
   return (
-    <UserContext.Provider value={{ token, setToken, setSignOut, getAuthHeader }}>
+    <UserContext.Provider
+      value={{ token, setToken, username, setSignOut, getAuthHeader }}
+    >
       <Route path="/signin" component={SignIn} />
       <Route path="/signup" component={SignUp} />
       <Route path="/signout" component={SignOut} />
