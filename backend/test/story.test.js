a6ff935a92c85d139200c74d07fdda3e6760ed6d
NuttySalmon <nuttysalmon@gmail.com>
2020-11-27 02:58:01 -0800

    Added tests for authentication backend.

---
+++ backend/test/story.test.js
@@ -0,0 +1,95 @@
+const db = require('../models')
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const app = require('../app')
+
+const should = chai.should()
+// const requrester = chai.request(app).keepOpen()
+chai.use(chaiHttp)
+let header = {}
+
+describe('Story', () => {
+  // create user and login
+  before(async () => {
+    await db.sequelize.sync()
+    const user = {
+      firstname: 'John',
+      lastname: 'Doe',
+      username: 'jdoe',
+      password: 'password',
+    }
+    await chai.request(app).post('/api/user/signup').send({ data: user })
+    result = await chai.request(app).post('/api/user/signin').send(user)
+    header.Authorization = `Bearer ${result.body.token}`
+  })
+  describe('/GET story', () => {
+    it('Get all users', done => {
+      chai
+        .request(app)
+        .get('/api/story/all')
+        .set(header)
+        .end((_err, res) => {
+          res.should.have.status(200)
+          res.body.should.be.a('array')
+          done()
+        })
+    })
+  })
+
+  describe('/CREATE story', () => {
+    it('Add story', done => {
+      const story = {
+        name: 'test story',
+      }
+      chai
+        .request(app)
+        .post('/api/story/create')
+        .send({ data: story })
+        .set(header)
+        .end((_err, res) => {
+          res.should.have.status(200)
+          res.body.should.be.a('object')
+          const data = res.body
+          data.should.have.property('id')
+          data.should.have.property('status')
+          data.should.have.property('name').eq(story.name)
+          data.should.have.property('status')
+          done()
+        })
+    })
+    it('return 400', done => {
+      chai
+        .request(app)
+        .post('/api/story/create')
+        .set(header)
+        .send({ data: {} })
+        .end((_err, res) => {
+          res.should.have.status(400)
+          done()
+        })
+    })
+  })
+
+  describe('/PUT update story status', () => {
+    const url = '/api/story/status-update'
+    it('update story status', async () => {
+      const story = {
+        name: 'test story',
+      }
+      // create story first
+      let res = await chai
+        .request(app)
+        .post('/api/story/create')
+        .set(header)
+        .send({ data: story })
+      const { id } = res.body
+      res = await chai
+        .request(app)
+        .put(url)
+        .set(header)
+        .send({ data: { id: id, status: 2 } })
+      res.should.have.status(200)
+      res.body.should.have.property('affectedRows').be.a('number').eq(1)
+    })
+  })
+})
---
+++ backend/test/user.test.js
@@ -0,0 +1,99 @@
+const db = require('../models')
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const app = require('../app')
+
+const should = chai.should()
+
+chai.use(chaiHttp)
+const user = {
+  firstname: 'John',
+  lastname: 'Doe',
+  username: 'jdoe',
+  password: 'password',
+}
+describe('User', () => {
+  before(async () => {
+    await db.sequelize.sync()
+  })
+  describe('/PUT create user', () => {
+    const url = '/api/user/signup'
+    it('create user', done => {
+      chai
+        .request(app)
+        .post(url)
+        .send({ data: user })
+        .end((err, res) => {
+          res.should.have.status(200)
+          done()
+        })
+    })
+
+    it('missing fields error', done => {
+      chai
+        .request(app)
+        .post(url)
+        .send({ data: {} })
+        .end((err, res) => {
+          res.should.have.status(400)
+          done()
+        })
+    })
+
+    it('user already exist error', done => {
+      chai
+        .request(app)
+        .post(url)
+        .send({ data: user })
+        .end((err, res) => {
+          chai
+            .request(app)
+            .post(url)
+            .send({})
+            .end((err, res) => {
+              res.should.have.status(400)
+              done()
+            })
+        })
+    })
+  })
+
+  describe('/put signin', () => {
+    // create user
+    before(async () => {
+      await chai.request(app).post('/api/user/signup').send({ data: user })
+    })
+    it('login successfully', done => {
+      chai
+        .request(app)
+        .post('/api/user/signin')
+        .send(user)
+        .end((err, res) => {
+          res.should.have.status(200)
+          done()
+        })
+    })
+
+    it('login fail with wrong password', done => {
+      chai
+        .request(app)
+        .post('/api/user/signin')
+        .send({ username: user.username, password: 'wrong' })
+        .end((err, res) => {
+          res.should.have.status(400)
+          done()
+        })
+    })
+
+    it('login fail with wrong email', done => {
+      chai
+        .request(app)
+        .post('/api/user/signin')
+        .send({ username: 'wrong', password: user.password })
+        .end((err, res) => {
+          res.should.have.status(404)
+          done()
+        })
+    })
+  })
+})
