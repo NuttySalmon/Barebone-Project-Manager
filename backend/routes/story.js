c61a3148622119bf9f31bf4d91858072f0906d5e
NuttySalmon <nuttysalmon@gmail.com>
2020-11-27 02:59:40 -0800

    Added authentication to story routes

--- backend/routes/story.js
+++ backend/routes/story.js
@@ -1,7 +1,9 @@
 var express = require('express')
 const { getAll, create, updateStatus } = require('../controllers/story')
+const passport = require('passport')
 var router = express.Router()
 
+router.all('*', passport.authenticate('jwt', { session: false }))
 router.post('/create', create)
 router.get('/all', getAll)
 router.put('/status-update', updateStatus)
