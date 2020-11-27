6d835b6b49ec1cc0bbc1caef63d20b549dcf114b
NuttySalmon <nuttysalmon@gmail.com>
2020-11-27 03:00:59 -0800

    Added Auth token getter sign out function in context

--- frontend/src/components/AuthService/index.js
+++ frontend/src/components/AuthService/index.js
@@ -2,17 +2,23 @@ import React, { useEffect, createContext, useState } from 'react'
 import SignIn from './SignIn'
 import SignOut from './SignOut'
 import SignUp from './SignUp'
-import { Route } from 'react-router-dom'
+import { Route, useHistory } from 'react-router-dom'
 
 export const UserContext = createContext()
 const AuthService = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem('token'))
+  const history = useHistory()
   useEffect(() => {
     localStorage.setItem('token', token)
   }, [token])
-
+  const setSignOut = () => {
+    setToken('')
+    localStorage.removeItem('token')
+    history.push('/signin')
+  }
+  const getAuthHeader = () => ({ Authorization: `Bearer ${token}` })
   return (
-    <UserContext.Provider value={{ token, setToken }}>
+    <UserContext.Provider value={{ token, setToken, setSignOut, getAuthHeader }}>
       <Route path="/signin" component={SignIn} />
       <Route path="/signup" component={SignUp} />
       <Route path="/signout" component={SignOut} />
