141ed8ceb1e376e44f4d84d5c893c42aa534fe03
NuttySalmon <nuttysalmon@gmail.com>
2020-11-27 03:51:43 -0800

    Moved key reading in Auth into try catch.

--- backend/auth.js
+++ backend/auth.js
@@ -1,19 +1,23 @@
 const { User } = require('./models')
 const fs = require('fs')
 const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt')
+const errorLog = require('debug')('passport:error')
 const opts = {
-  algorithm: ["RS256"],
+  algorithm: ['RS256'],
 }
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
 // opts.secretOrKey = 'secret'
-const privateKey = fs.readFileSync(`${process.env.KEY_DIR}/private.key`)
-const publicKey = fs.readFileSync(`${process.env.KEY_DIR}/public.pem`)
-opts.secretOrKey = publicKey
-process.env.PRIVATE_KEY = privateKey
 
+/** Setup passport with jwt */
 module.exports = passport => {
-  /** Passport setup  */
   try {
+    /** Read keys */
+    const privateKey = fs.readFileSync(`${process.env.KEY_DIR}/private.key`)
+    const publicKey = fs.readFileSync(`${process.env.KEY_DIR}/public.pem`)
+    opts.secretOrKey = publicKey
+    
+    /** Passport setup  */
+    process.env.PRIVATE_KEY = privateKey
     passport.use(
       new JwtStrategy(opts, async (jwt_payload, done) => {
         try {
