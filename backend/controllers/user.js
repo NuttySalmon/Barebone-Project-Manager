3db00bec5f91693f99c9ffb4a6e15f3d334b87ac
NuttySalmon <nuttysalmon@gmail.com>
2020-11-26 20:56:56 -0800

    Added sign in in backend.

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,8 +1,9 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { User } = require('../models')
+const fs = require('fs')
 
-exports.create = async (req, res) => {
+exports.signup = async (req, res) => {
   try {
     const newUser = req.body.data
     const existingUser = await User.findAll({
@@ -27,5 +28,45 @@ exports.create = async (req, res) => {
     })
   } catch (err) {
     return res.status(400).send(err)
+  }
+}
+
+exports.signin = async (req, res) => {
+  if (!req.body.username || !req.body.password)
+    return res.status(400).send('Username and password cannot be empty')
+
+  const { username, password } = req.body
+  const user = await User.findAll({ where: { username } })
+
+  //check for user
+  if (!user.length) return res.status(404).send('User not found')
+  let passwordHash = user[0].dataValues.password
+
+  //check for password
+  try {
+    const isMatch = await bcrypt.compare(password, passwordHash)
+    if (isMatch) {
+      // user matched
+      console.log('matched!')
+      const { id, username } = user[0].dataValues
+      const payload = { id, username } //jwt payload
+      // console.log(payload)
+      jwt.sign(payload, process.env.PRIVATE_KEY, { expiresIn: 3600, algorithm: 'RS256' }, (err, token) => {
+        if (err)
+          res.status(401).send({
+            success: false,
+            error: err,
+          })
+        else
+          res.send({
+            success: true,
+            token,
+          })
+      })
+    } else {
+      return res.status(400).send('Incorrect password')
+    }
+  } catch (error) {
+    return res.status(500).send(error)
   }
 }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router = express.Router()
 const debug = require('debug')('debug:user')
-const { create } = require('../controllers/user')
-router.post('/create', create)
+const { signup, signin } = require('../controllers/user')
+router.post('/signup', signup)
+router.post('/signin', signin)
 module.exports = router
