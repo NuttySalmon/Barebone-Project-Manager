b780b7f22a237b432984a642c9b1c494f536d884
NuttySalmon <nuttysalmon@gmail.com>
2020-11-30 09:28:40 -0800

    Fixed bugs in task controller and implemented find one task logic.

--- backend/controllers/task.js
+++ backend/controllers/task.js
@@ -1,67 +1,85 @@
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
 const { Task } = require('../models')
 const debug = require('debug')('debug:user-control')
 
 exports.create = async (req, res) => {
-    try {
-        const result = await Task.create(req.body, {
-          fields: ['name', 'complete'],
-        })
-        res.status(200).send(result.dataValues)
-      } catch (error) {
-        if (error.name === 'SequelizeValidationError') res.status(400).send(error)
-        res.status(500).send(error)
-      }
+  const { storyId, name } = req.body
+  if (!name) return res.status(400).send('Name cannot be empty')
+  if (storyId === undefined)
+    return res.status(400).send('Must include story id')
+
+  try {
+    const newTask = {
+      name,
+      StoryId: storyId,
+      complete: false,
+    }
+    const result = await Task.create(newTask)
+    res.status(500).send(result.dataValues)
+  } catch (error) {
+    if (error.name === 'SequelizeValidationError' || error.name === 'TypeError')
+      return res.status(400).send('Invalid name')
+    res.status(500).send(error.message)
+  }
 }
 
 exports.deleteTask = async (req, res) => {
-    try {
-        const {id} = req.body
-        const exitance = id =>
-            Task.findOne({ where: { id} })
-                .then(token => token !== null)
-                .then(isUnique => isUnique);
-        if (existance == false) res.status(400).send('Task does not exist')
-        const result = await Task.destroy({
-            where: { id }
-        })
-        res.status(200).send({affectedRows: result[0]})
-    } catch (error) {
-        debug(error)
-        console.log(error) 
-        if (error.name === 'SequelizeValidationError') res.status(400).send(error)
-        else res.sendStatus(500)
-    }
+  try {
+    const { id } = req.query
+    const found = id =>
+      Task.findOne({ where: { id } })
+        .then(token => token !== null)
+        .then(isUnique => isUnique)
+    if (found.length) return res.status(404).send('Task does not exist')
+    const result = await Task.destroy({
+      where: { id },
+    })
+    return res.status(200).send({ affectedRows: result[0] })
+  } catch (error) {
+    debug(error)
+    console.log(error)
+    if (error.name === 'SequelizeValidationError')
+      return res.status(400).send(error)
+    else res.sendStatus(500)
+  }
+}
+exports.findOne = async (req, res) => {
+  const { id } = req.query
+  if (id === undefined) return res.sendStatus(400)
+  try {
+    const result = await Task.findByPk(id)
+    res.status(200).send(result)
+  } catch (error) {
+    debug(error)
+    res.status(500).send(error)
+  }
 }
 
 exports.getAll = async (req, res) => {
-    try {
-        const result = await Task.findAll()
-        res.send(result).status(200)
-      } catch (error) {
-        debug(error)
-        res.status(500).send(error)
-      }
+  try {
+    const result = await Task.findAll()
+    res.send(result).status(200)
+  } catch (error) {
+    debug(error)
+    res.status(500).send(error)
+  }
 }
 
 exports.updateStatus = async (req, res) => {
-    try{
-        const {name, id, complete} = req.body
-        const newName = name
-        const newComplete = complete
-        if (typeof newName !== 'string') res.status(400).send('Invalid data type')
-        if (typeof newComplete !== 'boolean') res.status(400).send('Invalid data type')
-        const result = await Task.update(
-            { complete: newComplete },
-            { name: newName},
-            { where: { id } }
-        )
-        res.status(200).send({ affectedRows: result[0] }) 
-    } catch (error){
-        debug(error)
-        console.log(error) 
-        if (error.name === 'SequelizeValidationError') res.status(400).send(error)
-        else res.sendStatus(500)
-    }
+  try {
+    const { name, id, complete } = req.body
+    if (typeof name !== 'string')
+      return res.status(400).send('Invalid data type')
+    if (typeof complete !== 'boolean')
+      return res.status(400).send('Invalid data type')
+    const result = await Task.update(
+      { complete, name },
+      { where: { id } }
+    )
+    res.status(200).send({ affectedRows: result[0] })
+  } catch (error) {
+    debug(error)
+    console.log(error)
+    if (error.name === 'SequelizeValidationError') res.status(400).send(error)
+    else res.sendStatus(500)
+  }
 }
--- backend/routes/task.js
+++ backend/routes/task.js
@@ -1,12 +1,13 @@
 var express = require('express')
-const { getAll, create, updateStatus, deleteTask} = require('../controllers/task')
+const { getAll, findOne, create, updateStatus, deleteTask} = require('../controllers/task')
 const passport = require('passport')
 var router = express.Router()
 
 router.all('*', passport.authenticate('jwt', { session: false }))
 router.post('/create', create)
 router.get('/all', getAll)
-router.put('/status-update', updateStatus)
-router.delete('/delete-task', deleteTask)
+router.get('/find', findOne)
+router.put('/update', updateStatus)
+router.delete('/delete', deleteTask)
 
 module.exports = router
